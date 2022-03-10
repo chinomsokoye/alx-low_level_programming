a43a6a7649facfb877120b7a0e8a8583f0989541
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-10 07:28:33 -0800

    Print to the standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print to standard output
+ * Return: 0 if exited, compiled without error
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
