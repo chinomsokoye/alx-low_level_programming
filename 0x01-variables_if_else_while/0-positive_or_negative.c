318ab840de64e89712db6ffe8d5e21f6f5d47f35
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-11 05:47:43 -0800

    Assign random number to variable when executed

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print to standard output
+ * Return: 0 if exited, compiled without error
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+
+	return (0);
+}
