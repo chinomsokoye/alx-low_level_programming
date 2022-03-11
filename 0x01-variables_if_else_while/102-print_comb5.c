f4c73f3ac79244fae023b8d3426e680a55cb8979
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-11 14:19:40 -0800

    Print all possible combination of two digits ranging 0-99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,31 +11,21 @@ int main(void)
 {
 	int ten;
 	int one;
-	int th;
-	int onth;
 
-	for (ten = '0'; ten <= '9'; ten++)
+	for (ten = 0; ten <= 99; ten++)
 	{
-		for (one = '0'; one <= '9'; one++)
+		for (one = ten + 1; one <= 99; one++)
 		{
-			for (th = ten; th <= '9'; th++)
-			{
-				for (onth = one + 1; onth <= '9'; onth++)
-				{
-					putchar(ten);
-					putchar(one);
-					putchar(' ');
-					putchar(th);
-					putchar(onth);
+			putchar(ten / 10 + '0');
+			putchar(ten % 10 + '0');
+			putchar(' ');
+			putchar(one / 10 + '0');
+			putchar(one % 10 + '0');
 
-					if (!((ten == '9' && one == '8') &&
-					      (th == '9' && onth == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				onth = '0';
+			if (!(ten == 98 && one == 99))
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
