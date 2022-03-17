4dc6460b7e1ec65193988376ae26d2506655082b
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-17 07:02:01 -0700

    Draw a straight line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line in terminal
+ * @n: integer times to print to standard output
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
