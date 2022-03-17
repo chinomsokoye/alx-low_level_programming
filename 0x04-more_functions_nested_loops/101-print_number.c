7fea052d0b289baf258fec7a246623ff89e477a9
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-17 13:09:25 -0700

    Print an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	unsigned int i1;
+
+	if (n < 0)
+	{
+
+		i1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i1 = n;
+	}
+	if (i1 / 10)
+	{
+		print_number(i1 / 10);
+	}
+	_putchar((i1 % 10) + '0');
+}
