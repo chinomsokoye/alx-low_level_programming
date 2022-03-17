2f998864d5c280fb2d34617ac82df5c96fa9ea24
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-17 12:31:44 -0700

    Print number 1-100, in fizz buzz style

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-100
+ * multiples of 3 - fizz
+ * multiples of 5 - buzz
+ * multiples of both 3 and 5 - fizzbuzz
+ * Return: 0 if exited, compiled without errors
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
