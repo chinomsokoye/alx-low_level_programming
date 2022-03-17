ef0ec66d59e9e50279adbfda9f0640307e3fecb7
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-17 06:24:13 -0700

    Print numbers 0-9, newline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
