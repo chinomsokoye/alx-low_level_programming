b0bd55fbd0219f01e301c66b160d8e7fcd0606d4
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-21 04:49:22 -0700

    Print a string in reverse, newline

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse, newline
+ * @s: string character argument, parameter
+ */
+
+void print_rev(char *s)
+{
+	int a, b, len;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	len = a;
+
+	for (b = len - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+
+	_putchar('\n');
+}
