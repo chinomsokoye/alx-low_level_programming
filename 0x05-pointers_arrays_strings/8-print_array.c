8a1654c0d1290d1093b5042d828391616ac80f0a
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-21 05:55:51 -0700

    Print n elements of an array of integers, newline

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n element of an array of integers
@@ -13,15 +13,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == 0)
+		printf("%d", a[i]);
+		if (i != n - 1)
 		{
-			printf("%d", a[i]);
+			printf(", ");
 		}
-		else
-		{
-			printf(", %d", a[i]);
-		}
-
-		printf("\n");
 	}
+
+	printf("\n");
 }
