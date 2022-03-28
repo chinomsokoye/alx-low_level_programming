9755c7e345a7823e4bad52098daeb2bf648a364e
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-28 05:16:46 -0700

    Fill memory with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: string
+ * @b: byte
+ * @n: integer amouunt, number
+ * Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
