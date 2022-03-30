ef157130919e28dcce465e438f90a9cf100b5e9c
chinomso okoye <chinomsokoye@yahoo.com>
2022-03-30 13:32:54 -0700

    Return 1 if string is palindrome, 0 otherwise

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+int check_palindrome(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - returns 1 if string is palindrome, 0 otherwise
+ * @s: string character
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+	{
+		return (1);
+	}
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - return character for palindrome recursively
+ * @s: string
+ * @i: iterator argument, parameter
+ * @len: string length
+ * Return: 1 or 0
+ */
+
+int check_palindrome(char *s, int i, int len)
+{
+	if (*(s + 1) != *(s + len - 1))
+	{
+		return (0);
+	}
+	if (i >= len)
+	{
+		return (1);
+	}
+	return (check_palindrome(s, i + 1, len - 1));
+}
