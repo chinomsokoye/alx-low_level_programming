9221fe81141caa3715f6d994dd66af5a9815a919
chinomso okoye <chinomsokoye@yahoo.com>
2022-04-13 11:27:33 -0700

    Perform simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+
+/**
+ * main - simple operations program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	o = *argv[2];
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func(arg1, arg2);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers
+ * @a: integer
+ * @b: integer
+ * Return: subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: integer a
+ * @b: integer b
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two integers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of two integers
+ * @a: integer
+ * @b: integer
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
