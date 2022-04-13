1ebd6065d73eb95c93f3f769890d2186b278975a
chinomso okoye <chinomsokoye@yahoo.com>
2022-04-13 10:29:12 -0700

    Search for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,6 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * int_index - searches for ana iinteger
@@ -12,15 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	i = 0;
+	while (i < size)
 	{
-		i = 0;
-		while (i < size)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-			i++;
-		}
+		if (cmp(array[i]))
+			return (i);
+		i++;
 	}
 	return (-1);
 }
