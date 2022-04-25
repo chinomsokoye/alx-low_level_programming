934bf118313eb3f46685268a0ee828aa815d15ce
chinomso okoye <chinomsokoye@yahoo.com>
2022-04-25 11:24:35 -0700

    Free a listint_t list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,10 +9,13 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
+
 	while (head != NULL)
 	{
 		temp = head;
-		free(head);
 		head = temp->next;
+		free(head);
 	}
 }
