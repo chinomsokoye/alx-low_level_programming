15e41eccca878987de08da7fa4d10e5ce07eab54
chinomso okoye <chinomsokoye@yahoo.com>
2022-04-25 05:18:40 -0700

    Free a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: head, linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = *head;
+		free(*head);
+		*head = temp->next;
+	}
+	*head = NULL;
+}
