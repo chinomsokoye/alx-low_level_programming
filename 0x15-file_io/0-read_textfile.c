513f9974538a0053685b0c8229a85a4fd9914226
chinomso okoye <chinomsokoye@yahoo.com>
2022-05-02 06:57:46 -0700

    Read a textfile and print it to POSIX stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,12 +9,13 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, r, w;
+	int fd;
+	int r, w;
 	char *buf;
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
