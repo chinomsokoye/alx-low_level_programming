564f4ef49825c8d8d323f335879ba7704bf08bd3
chinomso okoye <chinomsokoye@yahoo.com>
2022-06-13 09:22:29 -0700

    Make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/chinomsokoye/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/chinomsokoye/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
