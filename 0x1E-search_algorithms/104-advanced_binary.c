54c02ab8709d6da8f7691ebde680da177036a180
chinomso okoye <chinomsokoye@yahoo.com>
2022-10-05 05:00:10 -0700

    Search for a value in a sorted array of integers

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -4,6 +4,35 @@
 #include <unistd.h>
 
 /**
+ * adv_binary_recursive - searches for a value recursively
+ * @array: Pointer to the first element of the array to search
+ * @left: Index left
+ * @right: Index right
+ * @value: Value to search for
+ * Return: index of value, -1 if absent
+ */
+int adv_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t i = left;
+
+	if (right < left)
+		return (-1);
+	printf("Searching in array: ");
+	while (i < right)
+	{
+		printf("%d, ", array[i]);
+		i++;
+	}
+	printf("%d\n", array[i]);
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (adv_binary_recursive(array, left, i, value));
+	return (adv_binary_recursive(array, i + 1, right, value));
+}
+
+/**
  * advanced_binary - searches for a value in a sorted array of integers
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
@@ -14,51 +43,5 @@ int advanced_binary(int *array, size_t size, int value)
 {
 	if (!array || size == 0)
 		return (-1);
-	return (binary_help(array, value, 0, size - 1));
-}
-
-/**
- * binary_help - searches for a value in an array
- * @array: Pointer to the first element of the array to search in
- * @value: Value to search for
- * @low: lower index value
- * @high: higher index value
- * Return: value of index found, -1 if absent
- */
-int binary_help(int *array, int value, size_t low, size_t high)
-{
-	size_t mid;
-
-	print_array(array, low, high);
-	if (high == low && array[low] != value)
-		return (-1);
-	mid = ((high - low) / 2) + low;
-	if (array[mid] == value)
-		return (mid);
-	if (array[mid] < value)
-		return (binary_help(array, value, mid + 1, high));
-	if (array[mid] > value)
-		return (binary_help(array, value, low, mid - 1));
-	return (-1);
-}
-
-/**
- * print_array - prints an array
- * @array: Pointer to the first element of the array
- * @low: lower index value
- * @high: higher index value
- */
-void print_array(int *array, size_t low, size_t high)
-{
-	size_t i = low;
-
-	printf("Searching in array: ");
-	while (i <= high)
-	{
-		printf("%d", array[i]);
-		if (i < high)
-			printf(", ");
-		i++;
-	}
-	printf("\n");
+	return (adv_binary_recursive(array, 0, size - 1, value));
 }
